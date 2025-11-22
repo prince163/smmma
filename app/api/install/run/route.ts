3dbbabe85b622d811720cafc3792c83720d0ae4d
prince163 <prince163@github.com>
2025-11-22 02:33:05 +0200

    Fix Edge Runtime compatibility - use Node.js runtime for install APIs

--- app/api/install/run/route.ts
+++ app/api/install/run/route.ts
@@ -1,3 +1,5 @@
+export const runtime = 'nodejs';
+
 import { NextResponse } from 'next/server';
 import { exec } from 'child_process';
 import { promisify } from 'util';
--- app/api/install/test-db/route.ts
+++ app/api/install/test-db/route.ts
@@ -1,3 +1,5 @@
+export const runtime = 'nodejs';
+
 import { NextResponse } from 'next/server';
 import mysql from 'mysql2/promise';
 
